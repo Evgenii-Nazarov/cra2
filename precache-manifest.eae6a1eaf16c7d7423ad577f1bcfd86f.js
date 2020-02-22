1826b6e247eb591b4a4c90810d30ec2db6e9def4
Evgenii Nazarov <ev66647260@gmail.com>
2020-02-22 09:59:43 -0800

    Updates

---
+++ precache-manifest.eae6a1eaf16c7d7423ad577f1bcfd86f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbc4b4927f35247c11379a32fbfba07",
+    "url": "/cra2/index.html"
+  },
+  {
+    "revision": "01ea2f3d2f072851af8f",
+    "url": "/cra2/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "4987e1078b6a5d04bb21",
+    "url": "/cra2/static/js/2.3728077b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra2/static/js/2.3728077b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ea2f3d2f072851af8f",
+    "url": "/cra2/static/js/main.182a6e9c.chunk.js"
+  },
+  {
+    "revision": "130af5c5c5c468c9a3c0",
+    "url": "/cra2/static/js/runtime-main.8a268de6.js"
+  }
+]);
